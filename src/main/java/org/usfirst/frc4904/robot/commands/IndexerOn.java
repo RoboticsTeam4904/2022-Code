116141569005564e00d3298513393f888fd03d2e
amrrao <amrrao@nuevaschool.org>
2022-01-30 10:14:17 -0800

    added on and off indexer commands

---
+++ src/main/java/org/usfirst/frc4904/robot/commands/IndexerOn.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc4904.robot.commands;
+
+import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
+
+public class IndexerOn extends MotorConstant {
+    public final static double DEFAULT_INTAKE_MOTOR_SPEED = 0.5; //TODO: needs value
+    public IndexerOn() {
+        super("IndexerOn", RobotMap.Component.motor, DEFAULT_INTAKE_MOTOR_SPEED);
+    }
+}
