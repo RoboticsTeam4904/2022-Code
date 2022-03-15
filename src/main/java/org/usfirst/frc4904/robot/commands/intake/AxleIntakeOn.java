9827791b857d3796dd66865ffe7f5c66220f3596
bcho04 <brandon.cho@gmail.com>
2022-03-14 17:43:50 -0700

    Fix shoot commands

--- src/main/java/org/usfirst/frc4904/robot/commands/intake/AxleIntakeOn.java
+++ src/main/java/org/usfirst/frc4904/robot/commands/intake/AxleIntakeOn.java
@@ -7,7 +7,7 @@ import org.usfirst.frc4904.standard.commands.motor.MotorConstant;
  * Rotates the axle to intake the ball
  */
 public class AxleIntakeOn extends MotorConstant {
-    public final static double DEFAULT_INTAKE_MOTOR_SPEED = 0.2; //TODO: needs value
+    public final static double DEFAULT_INTAKE_MOTOR_SPEED = 0.3; //TODO: needs value
     public AxleIntakeOn() {
         super("axleIntakeOn", RobotMap.Component.intakeAxleMotor, DEFAULT_INTAKE_MOTOR_SPEED);
     }
