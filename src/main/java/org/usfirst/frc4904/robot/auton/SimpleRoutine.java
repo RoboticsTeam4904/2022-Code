32dc77470a71dfa731cfecbf435c438ac418ed38
FlyN-Nick <43420132+FlyN-Nick@users.noreply.github.com>
2022-03-21 17:48:16 -0700

    Basic auton routine added, needs to be tuned.

--- src/main/java/org/usfirst/frc4904/robot/auton/SimpleRoutine.java
+++ src/main/java/org/usfirst/frc4904/robot/auton/SimpleRoutine.java
@@ -1,14 +1,17 @@
 package org.usfirst.frc4904.robot.auton;
 
 import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.robot.commands.indexerIntakeTurret.Shoot;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisConstant;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
 public class SimpleRoutine extends SequentialCommandGroup {
     public SimpleRoutine() {
+        // first backup, then shoot
         this.addCommands(
-            new ChassisConstant(RobotMap.Component.chassis, 0, -1, 0, 2)
+            new ChassisConstant(RobotMap.Component.chassis, 0, -1, 0, 2), // TODO: tune num of seconds, currently 2
+            new Shoot(-1).withTimeout(2) // TODO: tune 
         );
     }
 }
