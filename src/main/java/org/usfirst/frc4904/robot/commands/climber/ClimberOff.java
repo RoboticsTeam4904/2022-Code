cfa02d6c43501f68421a36e2175b8ed668e01843
zbuster05 <zakary.saya@gmail.com>
2022-03-23 23:59:28 -0700

    Ok turns out I need to add files

---
+++ src/main/java/org/usfirst/frc4904/robot/commands/climber/ClimberOff.java
@@ -0,0 +1,9 @@
+package org.usfirst.frc4904.robot.commands.climber;
+
+import org.usfirst.frc4904.robot.RobotMap;
+import org.usfirst.frc4904.standard.commands.motor.MotorIdle;
+public class ClimberOff extends MotorIdle {
+    public ClimberOff() {
+        super("ClimberOff", RobotMap.Component.climber.climberMotor);
+    }
+}
