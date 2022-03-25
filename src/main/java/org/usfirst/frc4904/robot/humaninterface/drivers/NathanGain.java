2c9b7a7607c61d29c68085e09ed0a7efb8e67f3b
roboticsteam4904-2 <102199775+roboticsteam4904-2@users.noreply.github.com>
2022-03-25 15:42:06 -0700

    lower drive constants (exp = 1)

--- src/main/java/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
+++ src/main/java/org/usfirst/frc4904/robot/humaninterface/drivers/NathanGain.java
@@ -9,9 +9,9 @@ import org.usfirst.frc4904.standard.commands.RunFor;
 import org.usfirst.frc4904.standard.humaninput.Driver;
 
 public class NathanGain extends Driver {
-	public static final double SPEED_GAIN = 1;
+	public static final double SPEED_GAIN = 0.9;
 	public static final double SPEED_EXP = 2;
-	public static final double TURN_GAIN = 0.7;
+	public static final double TURN_GAIN = 0.6;
 	public static final double TURN_EXP = 1;
 	public static final double Y_SPEED_SCALE = 1;
 	public static final double TURN_SPEED_SCALE = 1;
