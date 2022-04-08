61bf760b84f5233d8f74a1d1c42ac7d6efd9b577
roboticsteam4904-2 <102199775+roboticsteam4904-2@users.noreply.github.com>
2022-04-08 16:03:07 -0700

    Maybe an added to the auton routine?

--- src/main/java/org/usfirst/frc4904/robot/auton/SimpleRoutine.java
+++ src/main/java/org/usfirst/frc4904/robot/auton/SimpleRoutine.java
@@ -6,6 +6,7 @@ import org.usfirst.frc4904.robot.commands.indexer.IndexerSet;
 import org.usfirst.frc4904.robot.commands.indexerIntakeTurret.Shoot;
 import org.usfirst.frc4904.robot.commands.intake.ExtendIntake;
 import org.usfirst.frc4904.robot.commands.shooter.ShooterBrake;
+import org.usfirst.frc4904.robot.commands.shooter.ShooterConstant;
 import org.usfirst.frc4904.robot.commands.turret.TurnTurret;
 import org.usfirst.frc4904.robot.commands.turret.TurretAlign;
 import org.usfirst.frc4904.robot.subsystems.Indexer;
@@ -13,14 +14,24 @@ import org.usfirst.frc4904.robot.subsystems.Turret;
 import org.usfirst.frc4904.standard.commands.RunFor;
 import org.usfirst.frc4904.standard.commands.chassis.ChassisConstant;
 
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 
 public class SimpleRoutine extends SequentialCommandGroup {
     public SimpleRoutine(Turret turret) {
         // first backup, then shoot
         this.addCommands(
 
-            new ChassisConstant(RobotMap.Component.chassis, 0, -0.5, 0, 0.5) // TODO: tune num of seconds, currently 2
+            new ChassisConstant(RobotMap.Component.chassis, 0, -0.5, 0, 0.5), // TODO: tune num of seconds, currently 2
+            new ParallelCommandGroup(
+				new SequentialCommandGroup(
+					new WaitCommand(0.7), 
+					new RunFor(new IndexerSet(Indexer.DEFAULT_INDEXER_SPEED, -Indexer.DEFAULT_INDEXER_SPEED), 2), 
+					new IndexerOff()
+				),
+				new RunFor(new ShooterConstant(0.8), 5)
+			)
             //new RunFor(new ExtendIntake(), 2.2)
             //new TurnTurret(0.0),
             //new TurretAlign(RobotMap.Component.robotUDP, RobotMap.Component.turret),
